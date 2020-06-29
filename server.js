2ba159974f5503eb78604bc0a3becccde07657f5
guykem123 <50095264+guykem123@users.noreply.github.com>
2020-06-29 19:10:52 +0300

    update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/amcharts-Demo'));
+app.use(express.static(__dirname + '/dist/AmChartDemo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/amcharts-Demo/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AmChartDemo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
